2453b8391d1ab7f73fd80ae30ae6d59469a81ce3
Svetlin Simeonov <svetlin@simeonov.org>
2026-05-19 04:04:22 +0300

    Added karateka final melody from 1984 by Jordan Mechner

---
+++ src/games/karateka/karateka.ino
@@ -0,0 +1,73 @@
+// Karateka 1984 - Final Song
+// edited by by CDman
+
+#include "pitches.h"
+
+#define BUZZER_PIN 9
+
+// notes in the melody:
+int melody[] = {
+
+  NOTE_A3,NOTE_C4,NOTE_E3,NOTE_B3,
+  NOTE_C3,NOTE_A3,NOTE_E3,NOTE_B3,
+  NOTE_D3,NOTE_A3,NOTE_B3,NOTE_F4,
+  NOTE_E3,NOTE_GS3,NOTE_B3,NOTE_D4,
+  NOTE_CS3,NOTE_CS4,NOTE_GS3,NOTE_B3,
+  NOTE_FS3,NOTE_A3,NOTE_D3,NOTE_FS4,
+  NOTE_E3,NOTE_A3,NOTE_B3,NOTE_E4,
+  NOTE_E3,NOTE_GS3,NOTE_E4,NOTE_D4,
+  NOTE_CS4,NOTE_A3,NOTE_D4,NOTE_B3,
+  NOTE_E4,NOTE_CS4,NOTE_A3,NOTE_E3,
+  NOTE_CS3,NOTE_F3,NOTE_A3,NOTE_CS4,
+  NOTE_B3,NOTE_GS3,NOTE_F3,NOTE_B2,
+  NOTE_A2,NOTE_CS3,NOTE_FS3,NOTE_A3,
+  NOTE_D3,NOTE_FS3,NOTE_B3,NOTE_D4,
+  NOTE_E3,NOTE_A3,NOTE_D4,NOTE_CS4,
+  NOTE_E3,NOTE_GS3,NOTE_C4,NOTE_B3
+
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int durations[] = {
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4
+};
+
+void setup()
+{
+  pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void loop()
+{
+  int size = sizeof(durations) / sizeof(int);
+
+  for (int note = 0; note < size; note++) {
+    //to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int duration = 1000 / durations[note];
+    tone(BUZZER_PIN, melody[note], duration);
+
+    //to distinguish the notes, set a minimum time between them.
+    //the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = duration * 1.30;
+    delay(pauseBetweenNotes);
+    
+    //stop the tone playing:
+    noTone(BUZZER_PIN);
+  }
+}
